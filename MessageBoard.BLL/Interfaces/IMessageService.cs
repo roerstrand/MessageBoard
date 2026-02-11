a8d25aefac8d16c8553d675601c02ba63f0e10c2
roerstrand <129670382+roerstrand@users.noreply.github.com>
2026-02-11 18:57:05 +0100

    Refactor MessageService with async CRUD and DI support
    Refactored MessageService to use dependency injection for IMessageRepository and IUserRepository. Replaced placeholder methods with async CRUD operations for messages and users. Updated constructors to accept dependencies. Added new IMessageService.cs file as a placeholder, though it currently contains a class instead of an interface. Removed obsolete stub methods.

---
+++ MessageBoard.BLL/Interfaces/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBoard.BLL.Interfaces
+{
+    internal class IMessageService
+    {
+    }
+}
--- MessageBoard.BLL/Services/MessageService.cs
+++ MessageBoard.BLL/Services/MessageService.cs
@@ -1,30 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 
 namespace MessageBoard.BLL.Services
 {
     internal class MessageService
     {
-        public void OnGet()
-        {
+        private readonly IMessageRepository _messageRepository;
+        private readonly IUserRepository _userRepository;
 
+        public MessageService(
+            IMessageRepository messageRepository,
+            IUserRepository userRepository)
+        {
+            _messageRepository = messageRepository;
+            _userRepository = userRepository;
+        }
+        public MessageService(UserManager<IdentityUser> userManager)
+        {
         }
 
-        public Task<IActionResult> OnPostCreateMessage(Message message) { }
-
-        public Task<IActionResult> OnPostUpdateMessage(Message message){}
-
-        public Task<IActionResult> OnPostDeleteMessage(Message message) { }
-
-        public Task<IActionResult> OnPostDeleteUser(IdentityUser user) {}
-
-        public Task<IActionResult> OnPostUpdateUser(IdentityUser)
+        public async Task GetAllMessagesAsync()
         {
-            DatabaseService.UpdateUser(IDentityUser user)
+            await _messageRepository.GetAllMessagesAsync();
         }
 
-        
+        public async Task AddMessageAsync(Message message)
+        {
+            await _messageRepository.AddMessageAsync(message);
+        }
+
+        public async Task UpdateMessageAsync(Message message)
+        {
+            await _messageRepository.UpdateMessageAsync(message);
+        }
+
+        public async Task DeleteMessageAsync(Message message)
+        {
+            await _messageRepository.DeleteMessageAsync(message);
+        }
+
+        public async Task DeleteUserAsync(IdentityUser user)
+        {
+            await DBService.DeleteAsync(user);
+        }
+
+        public async Task UpdateUserAsync(IdentityUser user)
+        {
+            await DBService.UpdateAsync(user);
+        }
 
     }
 }
