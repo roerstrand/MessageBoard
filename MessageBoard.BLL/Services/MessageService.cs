83e207321098c97d892c78c0e80d612ae6d3e225
roerstrand <129670382+roerstrand@users.noreply.github.com>
2026-02-11 11:41:42 +0100

    k

---
+++ MessageBoard.BLL/Services/MessageService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBoard.BLL.Services
+{
+    internal class MessageService
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public Task<IActionResult> OnPostCreateMessage(Message message) { }
+
+        public Task<IActionResult> OnPostUpdateMessage(Message message){}
+
+        public Task<IActionResult> OnPostDeleteMessage(Message message) { }
+
+        public Task<IActionResult> OnPostDeleteUser(IdentityUser user) {}
+
+        public Task<IActionResult> OnPostUpdateUser(IdentityUser)
+        {
+            DatabaseService.UpdateUser(IDentityUser user)
+        }
+
+        
+
+    }
+}
