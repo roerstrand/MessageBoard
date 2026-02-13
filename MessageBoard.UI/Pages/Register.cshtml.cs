02f9bbde479e84710a0af69a2a67789494254267
henriksih <henrik.sihvonen@gmail.com>
2026-02-13 09:51:06 +0100

    login and Register functionality added. Not finished yet.

--- MessageBoard.UI/Pages/Register.cshtml.cs
+++ MessageBoard.UI/Pages/Register.cshtml.cs
@@ -1,12 +1,58 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace MessageBoard.UI.Pages
 {
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [BindProperty, Required]
+        public string Username { get; set; }
+
+
+        [BindProperty, Required]
+        public string Password { get; set; }
+
+
+        [BindProperty, Compare(nameof(Password))]
+
+        public string ConfirmPassword { get; set; }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = new IdentityUser { UserName = Username };
+            var result = await _userManager.AddPasswordAsync(user, Password);
+
+            if (result.Succeeded)
+            {
+                //await _signInManager.SignInAsync(user, false);
+                return RedirectToPage("/Login");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return Page();
+
+            //public void OnGet()
+            //{
+            //}
         }
     }
 }
