3fe8860b600a7c4dd92b099713dfac744498c71e
henriksih <henrik.sihvonen@gmail.com>
2026-02-13 13:27:16 +0100

    Updated Program with IMessageService

--- MessageBoard.UI/Program.cs
+++ MessageBoard.UI/Program.cs
@@ -1,5 +1,7 @@
 using MessageBoard.BLL.DTOs;
 using MessageBoard.BLL.Interfaces;
+using MessageBoard.BLL.Services;
+
 //using MessageBoard.BLL.Services;
 using MessageBoard.DLL.Data;
 using MessageBoard.DLL.Entities;
@@ -13,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-//builder.Services.AddScoped<IMessageService, MessageService>();
+builder.Services.AddScoped<IMessageService, MessageService>();
 builder.Services.AddScoped<IMessageRepository, MessageRepository>();
 //builder.Services.AddScoped<IUserRepository, UserRepository>();
 
